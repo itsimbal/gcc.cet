1a1ee12596d4e8cd76d2201f47fb35a2bb054713
H.J. Lu <hjl.tools@gmail.com>
2017-07-08 16:29:08 -0700

    Add check_effective_target_cet
    1. Add check_effective_target_cet.
    2. Rename cet-sjlj-2.c to cet-sjlj-3.c.
    3. Update cet-sjlj-1.c.
    4. Add a run-time test for cet-sjlj-1.c.

--- gcc/testsuite/gcc.target/i386/cet-sjlj-1.c
+++ gcc/testsuite/gcc.target/i386/cet-sjlj-1.c
@@ -4,42 +4,38 @@
 /* { dg-final { scan-assembler-times "rdssp\[dq]" 2 } } */
 /* { dg-final { scan-assembler-times "incssp\[dq]" 1 } } */
 
-#include <stdio.h>
-#include <setjmp.h>
+/* Based on gcc.dg/setjmp-3.c.  */
 
-jmp_buf buf;
-int bar (int);
+void *buf[5];
 
-int
-foo (int i)
+extern void abort (void);
+
+void raise0(void)
 {
-  int j = i * 11;
-
-  if (!__builtin_setjmp (buf))
-    {
-      j += 33;
-      printf ("After setjmp: j = %d\n", j);
-      bar (j);
-    }
-
-  return j + i;
-}
-
-int
-bar (int i)
-{
-int j = i;
-
-  j -= 111;
-  printf ("In longjmp: j = %d\n", j);
   __builtin_longjmp (buf, 1);
-
-  return j;
 }
 
-int
-main ()
+int execute(int cmd)
 {
-  foo (10);
+  int last = 0;
+
+  if (__builtin_setjmp (buf) == 0)
+    while (1)
+      {
+	last = 1;
+	raise0 ();
+      }
+
+  if (last == 0)
+    return 0;
+  else
+    return cmd;
+}
+
+int main(void)
+{
+  if (execute (1) == 0)
+    abort ();
+
   return 0;
 }
--- gcc/testsuite/gcc.target/i386/cet-sjlj-2.c
+++ gcc/testsuite/gcc.target/i386/cet-sjlj-2.c
@@ -1,45 +1,4 @@
-/* { dg-do compile } */
+/* { dg-do run { target cet } } */
 /* { dg-options "-O -finstrument-control-flow -mcet" } */
-/* { dg-final { scan-assembler-times "endbr32|endbr64" 4 } } */
-/* { dg-final { scan-assembler-times "call	_setjmp" 1 } } */
-/* { dg-final { scan-assembler-times "call	longjmp" 1 } } */
 
-#include <stdio.h>
-#include <setjmp.h>
-
-jmp_buf buf;
-int bar (int);
-
-int
-foo (int i)
-{
-  int j = i * 11;
-
-  if (!setjmp (buf))
-    {
-      j += 33;
-      printf ("After setjmp: j = %d\n", j);
-      bar (j);
-    }
-
-  return j + i;
-}
-
-int
-bar (int i)
-{
-int j = i;
-
-  j -= 111;
-  printf ("In longjmp: j = %d\n", j);
-  longjmp (buf, 1);
-
-  return j;
-}
-
-int
-main ()
-{
-  foo (10);
-  return 0;
-}
+#include "cet-sjlj-1.c"
