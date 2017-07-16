6bbd54e0fab061c4b3c5f9c4fef4126f3dbf16c2
H.J. Lu <hjl.tools@gmail.com>
2017-07-16 15:14:37 -0700

    Add 2 generic notrack tests
    1. Add 2 generic notrack tests.
    2. Rename noncompile/finstrument-control-flow-option.c to
       finstrument-control-flow.c.
    3. Remove gcc.target/i386/cet-option.c which is the same as
       gcc.dg/finstrument-control-flow.c.

--- gcc/testsuite/gcc.target/i386/cet-option.c
+++
@@ -1,3 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-finstrument-control-flow" } */
-/* { dg-error "'-finstrument-control-flow' requires CET support on this target. Use -mcet or one of -mibt, -mshstk options to enable CET." "" { target { "i?86-*-* x86_64-*-*" } } 0 } */
