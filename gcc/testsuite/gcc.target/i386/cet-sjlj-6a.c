87361366f6014af734a1b1bd474d0df5ac9c1e3d
Igor Tsimbalist <igor.v.tsimbalist@intel.com>
2018-01-30 16:42:42 +0300

    PR84066 Wrong shadow stack register size is saved for x32
    x32 is a 64-bit process with 32-bit software pointer and kernel may
    place x32 shadow stack above 4GB.  We need to save and restore 64-bit
    shadow stack register for x32. builtin jmp buf size is 5 pointers.  We
    have space to save 64-bit shadow stack pointers: 32-bit SP, 32-bit FP,
    32-bit IP, 64-bit SSP for x32.
    
    	PR target/84066
    	* gcc/config/i386/i386.md: Replace Pmode with word_mode in
    	builtin_setjmp_setup and builtin_longjmp to support x32.
    	* gcc/testsuite/gcc.target/i386/cet-sjlj-6a.c: New test.
    	* gcc/testsuite/gcc.target/i386/cet-sjlj-6b.c: Likewise.

---
+++ gcc/testsuite/gcc.target/i386/cet-sjlj-6a.c
@@ -0,0 +1,20 @@
+/* { dg-do compile } */
+/* { dg-options "-O -maddress-mode=short -fcf-protection -mcet -mx32" } */
+/* { dg-final { scan-assembler-times "endbr64" 2 } } */
+/* { dg-final { scan-assembler-times "movq\t.*buf\\+12" 1 } } */
+/* { dg-final { scan-assembler-times "subq\tbuf\\+12" 1 } } */
+/* { dg-final { scan-assembler-times "shrl\t\\\$3," 1 } } */
+/* { dg-final { scan-assembler-times "rdsspq" 2 } } */
+/* { dg-final { scan-assembler-times "incsspq" 2 } } */
+
+void *buf[5];
+
+void raise0(void)
+{
+  __builtin_longjmp (buf, 1);
+}
+
+void execute(int cmd)
+{
+  __builtin_setjmp (buf);
+}
