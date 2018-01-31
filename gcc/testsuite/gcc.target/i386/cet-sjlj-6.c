2c7f22ebe836210167ba72221a619dc9c76e8468
Igor Tsimbalist <igor.v.tsimbalist@intel.com>
2018-01-31 17:50:12 +0300

    Add a check to the testcase

--- gcc/testsuite/gcc.target/i386/cet-sjlj-6.c
+++ gcc/testsuite/gcc.target/i386/cet-sjlj-6.c
@@ -3,6 +3,7 @@
 /* { dg-final { scan-assembler-times "endbr64" 2 } } */
 /* { dg-final { scan-assembler-times "movq\t.*buf\\+12" 1 } } */
 /* { dg-final { scan-assembler-times "subq\tbuf\\+12" 1 } } */
+/* { dg-final { scan-assembler-times "shrl\t\\\$3" 1 } } */
 /* { dg-final { scan-assembler-times "rdsspq" 2 } } */
 /* { dg-final { scan-assembler-times "incsspq" 2 } } */
 
